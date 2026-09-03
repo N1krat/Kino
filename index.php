8f1e5b0a168968f9cb2b9006a138cfc829349581
N1krat <bogdanbraniste2007@gmail.com>
2026-09-03 22:03:23 +0300

    Ver 1.1

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <?php
 
-$denumireProiect = "Sistem de gestionare a unui cinematograf";
+$denumireProiect = "Kino - Sistem de gestionare a unui cinematograf";
 $autor = "Braniște Bogdan";
 $grupa = "AAW-231";
-$entitate = "Student";
 $descriere = "Aplicație pentru gestionarea filmelor și 
-sesiunilor de vizionare a acestora";
+sesiunilor de vizionare a acestora, unde noi putem vizualiza ce filme sunt 
+in rulare si de a procura bilete pentru ele";
+$entitatilePlanificate = "Film, Utilizator, Administrator";
 
-const VERSIUNE_APLICATIE = "1.0";
+# Prima entitate
+# Film 1
+$titlu = "Project Hail Mary";
+$regizor = "Phil Ford";
+$PretulSesiunii = 75.00;
+$NrLocuriDisponibile = 34;
+$OraInceperiiSesiunii = "18:00";
 
+
+
+const VERSIUNE_APLICATIE = "1.1";
 ?>
